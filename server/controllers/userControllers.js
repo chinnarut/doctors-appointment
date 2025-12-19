379e96f4d40d699917f9c6c2fa8ef9305cf2f6b5
Chinnarut <chinnarut.t@gmail.com>
2025-12-19 14:37:33 +0700

    add stripe webhooks

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -356,3 +356,42 @@ export const stripePayment = async (req, res) => {
     return res.status(500).json({ success: false, message: "Server Error." });
   }
 };
+
+// handle stripe webhooks
+export const stripeWebhooks = async (req, res) => {
+  const stripeInstance = new stripe(process.env.STRIPE_SECRET_KEY);
+  const sig = req.headers["stripe-signature"];
+  let event;
+
+  try {
+    event = stripeInstance.webhooks.constructEvent(
+      req.body,
+      sig,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    return res.status(400).send(`Webhook Error : ${error.message}`);
+  }
+
+  // handle the event
+  if (event.type === "payment_intent.succeeded") {
+    const paymentIntent = event.data.object;
+    const paymentIntentId = paymentIntent.id;
+
+    // getting session metadata
+    const session = await stripeInstance.checkout.sessions.list({
+      payment_intent: paymentIntentId,
+    });
+
+    const { appointmentId } = session.data[0].metadata;
+
+    // mark payment as paid
+    await Appointment.findByIdAndUpdate(appointmentId, {
+      payment: true,
+    });
+  } else {
+    console.log(`Unhandled event type ${event.type}`);
+  }
+
+  return res.status(200).json({ received: true });
+};
