57d8ce0506061915c45de097273c539e30ff46f3
Chinnarut <chinnarut.t@gmail.com>
2025-12-19 14:07:09 +0700

    show 6 top doctors in home page

--- client/src/components/TopDoctors.jsx
+++ client/src/components/TopDoctors.jsx
@@ -15,7 +15,7 @@ const TopDoctors = () => {
       </p>
       <div className="w-full grid grid-cols-(--grid-cols-auto) gap-4 pt-5 gap-y-6 px-3 sm:px-0">
         {doctors
-          .slice(0, 7)
+          .slice(0, 6)
           .reverse()
           .map((doctor, index) => (
             <div
